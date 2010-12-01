8fe4a4937d45a0b0f2ac9d21d96ab69bced0c01a
Joseph Mastey <jmmastey@gmail.com>
2010-11-30 22:21:11 -0600

    Forgot to add override last time. Update for formatting of controller and layout recipes.

---
+++ data/controller/Override.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * %FILENAME%
+ * 
+ * Controller file.
+ * 
+ * @author      %AUTHOR%
+ * @author      $Author$
+ * @version     $Id$
+ * @package     %PACKAGE%
+ * @subpackage  %PLUGIN%
+ * @copyright   Copyright (c) %COPYRIGHT%
+ */
+
+/**
+ * %DESCRIPTION%
+ * (Override of Mage_%PARENT_MODULE%_%PARENT_CONTROLLER%Controller)
+ *
+ * @author      %AUTHOR%
+ * @author      $Author$
+ * @package     %PACKAGE%
+ * @subpackage  %PLUGIN%
+ * @version     $Id$
+ * @copyright   Copyright (c) %COPYRIGHT%
+ * 
+ * @name        %CLASS%
+ *
+ */
+require_once("app/code/core/Mage/%PARENT_MODULE%/controllers/%PARENT_CONTROLLER%Controller.php");
+class %CLASS% extends Mage_%PARENT_MODULE%_%PARENT_CONTROLLER%Controller {
+
+  // nothing to see here
+
+}//class %CLASS%
