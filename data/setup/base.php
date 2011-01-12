ecb1665836d4c85de9c5757e0fbf22f9bbf658a2
Joseph Mastey <jmmastey@gmail.com>
2011-01-11 19:21:10 -0800

    Fixing setup routines

--- data/setup/base.php
+++ data/setup/base.php
@@ -3,14 +3,17 @@
 $installer = $this;
 
 
-// install by class
+// install by class. you will probably
+// need to define your setup class in the config
+// file and have that class descend from
+// eav/entity_setup
 //$installer->installEntities();
 
 
 // run DDL directly
 $installer->startSetup();
-$installer->run("
+/*$installer->run("
     ADD DDL STATEMENTS HERE
-");
+");*/
 $installer->endSetup(); 
-
+                                                                                          
