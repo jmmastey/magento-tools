898d0df756118906f88ba423f364d29f7dd509c5
Joseph Mastey <jmmastey@gmail.com>
2011-01-16 14:29:47 -0600

    more view updates. it lost the configurable products now, though

--- data/views/product_purchases.sql
+++ data/views/product_purchases.sql
@@ -14,22 +14,28 @@ select count(*) entries, cp.sku, oi.product_id, oi.name,
     cast(oi.created_at as date) purchase_date,
     attribute_set_name, cpi.price, cpi.min_price,
     (oi.price-oi.discount_amount) purchase_price,
+    cost.value cost,
     (case
-      when cp.visibility = 1 then 'Not Visible'
-      when cp.visibility = 2 then 'Catalog'
-      when cp.visibility = 3 then 'Search'
-      when cp.visibility = 4 then 'Catalog/Search'
+      when vis.value = 1 then 'Not Visible'
+      when vis.value = 2 then 'Catalog'
+      when vis.value = 3 then 'Search'
+      when vis.value = 4 then 'Catalog/Search'
     end) visibility,
-    (case when st.value = 1 then 'Enabled' else 'Disabled' end) enabled,
+    (case
+      when st.value = 1 then 'Enabled'
+      else 'Disabled'
+    end) enabled,
     group_concat(distinct ccev.value) event_names
   from sales_flat_order_item oi
     join sales_flat_order o on oi.order_id = o.entity_id
-    join catalog_product_flat_1 cp on cp.entity_id = oi.product_id
-    join eav_attribute_set ast on ast.attribute_set_id = cp.attribute_set_id
+    left join catalog_product_entity cp on cp.entity_id = oi.product_id
+    left join eav_attribute_set ast on ast.attribute_set_id = cp.attribute_set_id
     left join purchase_product_supplier pps on pps.pps_product_id = oi.product_id
     left join purchase_supplier ps on ps.sup_id = pps.pps_supplier_num
     left join catalog_product_index_price cpi on cpi.entity_id = oi.product_id
     left join catalog_product_entity_int st on st.entity_id = cp.entity_id
+    left join catalog_product_entity_int vis on vis.entity_id = cp.entity_id
+    left join catalog_product_entity_decimal cost on cost.entity_id = cp.entity_id
     left join catalog_category_product ccp on ccp.product_id = cp.entity_id
     left join catalog_category_entity cce on cce.entity_id = ccp.category_id
     left join catalog_category_entity_varchar ccev on ccev.entity_id = ccp.category_id
@@ -39,11 +45,13 @@ select count(*) entries, cp.sku, oi.product_id, oi.name,
     cpi.website_id = 1 and
     cpi.tax_class_id = 2 and
     st.attribute_id = 84 and
-    ccev.attribute_id = 33
+    ccev.attribute_id = 33 and
+    vis.attribute_id = 91 and
+    cost.attribute_id = 68
   group by
     oi.sku, oi.product_id, oi.name, ps.sup_name, cast(oi.created_at as date),
-    attribute_set_name, cpi.price, cpi.min_price, oi.price, oi.discount_amount,
-    cp.visibility, st.value, oi.product_type
+    attribute_set_name, cpi.price, cpi.min_price, cost.value, oi.price,
+    oi.discount_amount, vis.value, st.value, oi.product_type
   order by
     event_names asc, sku asc, purchase_date desc
 ;
